792a87dcdebe3e0a3581e762840855599ebbdf42
KeiDii <keidii@gmail.com>
2020-12-06 16:27:30 +0100

    description added

--- syslog_parser.py
+++ syslog_parser.py
@@ -7,15 +7,19 @@ if __name__ == '__main__':
   abnf_data = open('syslog.abnf','r').read()
   rules = abnf_parser.process_rules(abnf_data)
 
+  # get the definition
   test = rules['SYSLOG-MSG']
 
   #for name,value in rules.items():
   #  print(name)
 
-  #print(test)
+  # print the XML-like definition 
+  print(test)
 
+  # example how to override generated value
   rules['UTF-8-STRING'].validate = lambda x: b'hack!'
 
+  # generate few payloads 
   for i in range(1):
     print(test.generate())
 
